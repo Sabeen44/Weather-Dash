8739bbf96c2053ac14eb974b1fe631d49f6c200b
Beenz <CH.SABEEN@GMAIL.COM>
2023-04-10 22:17:03 -0700

    JS initiated and global variables for html elements created

---
+++ script.js
@@ -0,0 +1,7 @@
+var currentContainer = document.getElementById("current-weather");
+var submitButton = document.getElementById("submit-button");
+var formSubmit = document.getElementById("search-city");
+var formInput = document.getElementById("city-name");
+var forecastList = document.getElementById("forecast-list");
+var forecastContainer = document.getElementById("forecast-weather");
+var cityButtons = document.getElementById("buttons");
